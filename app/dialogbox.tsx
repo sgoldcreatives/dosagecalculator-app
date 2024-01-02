d04fb280fb73d63a3064a5b4c1b0f700bd09b7eb
Saar Gold <46770539+sgoldcreatives@users.noreply.github.com>
2024-01-01 20:30:20 -0800

    Update dialogbox.tsx

--- app/dialogbox.tsx
+++ app/dialogbox.tsx
@@ -13,17 +13,22 @@ export function DialogBox() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="ml-5 bg-violet-100 border-2 border-violet-300">
+        <Button
+          variant="outline"
+          className="ml-5 bg-violet-100 border-2 border-violet-300"
+        >
           Edit Tag Filters
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle className="text-slate-500">Hi! I don't do anything yet, but I will....</DialogTitle>
+          <DialogTitle className="text-slate-500">
+            Hi! I don&apos;t do anything yet, but I will....
+          </DialogTitle>
           <DialogTitle>Edit Tag Filters</DialogTitle>
           <DialogDescription>
-            Choose which tags to show in the table. Click submit when you're
-            done.
+            Choose which tags to show in the table. Click submit when
+            you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
