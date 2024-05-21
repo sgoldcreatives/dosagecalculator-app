f5461f232b5a72419f8002dfd929ad98da7fac8e
Saar Gold <46770539+sgoldcreatives@users.noreply.github.com>
2024-05-21 12:31:30 -0700

    Update plumbspdf.tsx

--- app/components/plumbspdf.tsx
+++ app/components/plumbspdf.tsx
@@ -2,11 +2,13 @@ export function PlumbsPdf() {
   return (
     <div className='flex'>
       {/* Display PDF in iframe */}
-      <iframe
-        src="/Plumbs-veterinary-drug-handbook.pdf"
-        width="200%"
-        height="800px"
-      />
+      <div className='w-16'>
+        <iframe
+          src="/Plumbs-veterinary-drug-handbook.pdf"
+          width="1500%"
+          height="800px"
+        />
+      </div>
       <h1 className="bg-violet-300 ml-4 h-20 text-white border-violet-400 border-2 rounded-md ">
         <strong>Hint:</strong> press ctrl + F to search
       </h1>
